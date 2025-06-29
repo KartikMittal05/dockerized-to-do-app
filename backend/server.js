481b296de24a6779d375ceb31a5d953699aef900
Kartik Mittal <kartik.22b0121110@abes.ac.in>
2025-06-30 01:35:23 +0530

    created backend

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import Todo from './models/todo.route.js';
+import {connectDB} from './config/db.js'; // Adjust the path as necessary
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json()); // Middleware to parse JSON bodies
+
+app.use("/api/todos",todoRoutes);
+
+
+app.listen(5000, () => {
+    connectDB(); // Ensure the database connection is established
+    console.log("Server started at http://localhost:5000");
+});
